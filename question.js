9dafa235435a9c7e5b514e95222a181c7fe6fa68
thetravisweber <tbroweber@gmail.com>
2022-09-24 11:38:14 -0700

    first commit; for james to style

---
+++ question.js
@@ -0,0 +1,47 @@
+const quizletIds = [705201314];
+
+main();
+
+async function main()
+{
+  let quizletId = randomElementFrom(quizletIds);
+  let cards = await fetchQuizletTerms(quizletId);
+  let testCard = randomElementFrom(cards);
+  let term = getTermFromCard(testCard);
+  let definition = getDefinitionFromCard(testCard);
+  console.log(term, definition);
+}
+
+function getTermFromCard(card)
+{
+  return card.cardSides[0].media[0].plainText;
+}
+
+function getDefinitionFromCard(card)
+{
+  return card.cardSides[1].media[0].plainText;
+}
+
+function randomElementFrom(arr)
+{
+  let randomIndex = Math.floor(arr.length * Math.random());
+  return arr[randomIndex];
+}
+
+async function fetchQuizletTerms(id){
+  let res = await fetch(
+    `https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D=${id}&filters%5BstudiableContainerType%5D=1&perPage=5&page=1`
+  ).then(res => res.json())
+  let currentLength = 5;
+  let token = res.responses[0].paging.token
+  let terms = res.responses[0].models.studiableItem;
+  let page = 2;
+  console.log({token, terms})
+  while (currentLength >= 5){
+      let res = await fetch(`https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D=${id}&filters%5BstudiableContainerType%5D=1&perPage=5&page=${page++}&pagingToken=${token}`).then(res => res.json());
+      terms.push(...res.responses[0].models.studiableItem);
+      currentLength = res.responses[0].models.studiableItem.length;
+      token = res.responses[0].paging.token;
+  }
+  return terms;
+}
